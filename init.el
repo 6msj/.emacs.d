e55b98ec75918ba5fd7f5fe958199378c4edd827
James Nguyen <james@whoshere.net>
2016-02-15 14:09:39 -0800

    testing

--- init.el
+++ init.el
@@ -193,16 +193,16 @@
 ;;;; Begin Experience
 
 ;; scroll by 1 line at the end of the file
-(setq scroll-step 1
-      scroll-conservatively 10000)
-;; set mouse wheel to scroll one line at a time
-(setq mouse-wheel-progressive-speed nil)
-;; scroll one line at a time (less "jumpy" than defaults)
-(setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
-;; don't accelerate scrolling
-(setq mouse-wheel-progressive-speed nil)
-;; scroll window under mouse
-(setq mouse-wheel-follow-mouse 't)
+;;(setq scroll-step 1
+;;      scroll-conservatively 10000)
+;;;; set mouse wheel to scroll one line at a time
+;;(setq mouse-wheel-progressive-speed nil)
+;;;; scroll one line at a time (less "jumpy" than defaults)
+;;(setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
+;;;; don't accelerate scrolling
+;;(setq mouse-wheel-progressive-speed nil)
+;;;; scroll window under mouse
+;;(setq mouse-wheel-follow-mouse 't)
 
 (require 'smooth-scrolling)
 (set-variable 'smooth-scroll-margin 5)
@@ -249,6 +249,11 @@
 (add-hook 'csharp-mode-hook 'omnisharp-mode)
 ;; https://stackoverflow.com/questions/29382137/omnisharp-on-emacs-speed
 (setq omnisharp-eldoc-support nil) ; disable for speed
+
+;; ios
+(add-hook 'objc-mode-hook
+          (lambda ()
+            (set (make-local-variable 'company-backends) '(company-xcode))))
 
 ;;;; End Completion
 
