f06b805eeed7c633fda52db9b4218677b0116f25
James Nguyen <james@whoshere.net>
2016-02-22 09:42:27 -0800

    increase height thresold for splits

--- init.el
+++ init.el
@@ -226,7 +226,7 @@
 ;; prefer vertical splits
 ;; https://stackoverflow.com/questions/2081577/setting-emacs-split-to-horizontal
 (setq split-height-threshold nil)
-(setq split-width-threshold 100)
+(setq split-width-threshold 200)
 
 ;; scroll by 1 line at the end of the file
 ;;(setq scroll-step 1
