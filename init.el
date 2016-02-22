d7d6c5caec1df77ceefdec6c1f8383002694ee24
James Nguyen <james@whoshere.net>
2016-02-22 14:50:20 -0800

    remove keymap

--- init.el
+++ init.el
@@ -561,7 +561,6 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
   (add-hook 'neotree-mode-hook
             (lambda ()
               (define-key evil-normal-state-local-map (kbd "TAB") 'neotree-enter)
-              (define-key evil-normal-state-local-map (kbd "SPC") 'neotree-enter)
               (define-key evil-normal-state-local-map (kbd "q") 'neotree-hide)
               (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter))))
 
