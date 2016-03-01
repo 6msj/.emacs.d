734709edaf0e24ad7540c754e6c01804cc080d8c
James Nguyen <james@whoshere.net>
2016-03-01 12:16:59 -0800

    tweak commenter keybinds

--- init.el
+++ init.el
@@ -573,13 +573,12 @@
       "m"  'multi-term
 
       ;; evil-nerd-commenter
-      "ci" 'evilnc-comment-or-uncomment-lines
+      "cc" 'evilnc-comment-or-uncomment-lines
       "cl" 'evilnc-quick-comment-or-uncomment-to-the-line
-      "cc" 'evilnc-copy-and-comment-lines
+      "ci" 'evilnc-copy-and-comment-lines
       "cp" 'evilnc-comment-or-uncomment-paragraphs
       "cr" 'comment-or-uncomment-region
       "cv" 'evilnc-toggle-invert-comment-line-by-line
-      "\\" 'evilnc-comment-or-uncomment-lines
       "co" 'evilnc-comment-operator
 
       ;; magit
