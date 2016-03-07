1184955949b25426ef090e945cc6ae259a03bc64
James Nguyen <james@whoshere.net>
2016-03-07 10:21:38 -0800

    delete unused binds

--- init.el
+++ init.el
@@ -340,11 +340,6 @@
 (global-set-key "\C-x\C-m" 'execute-extended-command)
 (global-set-key "\C-c\C-m" 'execute-extended-command)
 
-;; ways to delete word backwards
-(global-set-key "\C-w" 'backward-kill-word)
-(global-set-key "\C-x\C-k" 'kill-region)
-(global-set-key "\C-c\C-k" 'kill-region)
-
 (global-set-key (kbd "C-c o") 'occur) ;; occur!!
 (global-set-key (kbd "C-x C-r") 'rename-current-buffer-file) ;; rename
 
