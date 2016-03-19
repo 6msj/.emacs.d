a24effa9e99cd6f383c66e0902f87c1a0817933c
James Nguyen <james@whoshere.net>
2016-03-19 15:04:55 -0700

    increase recent item count

--- init.el
+++ init.el
@@ -521,7 +521,7 @@
   :config
   (setq recentf-auto-cleanup 'never) ;; disable before we start recentf!
   (recentf-mode 1)
-  (setq recentf-max-menu-items 125))
+  (setq recentf-max-menu-items 325))
 
 ;;;; End File Management
 
