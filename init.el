765b221db1eabd611b24f0b5a44037881c03a545
James Nguyen <james@whoshere.net>
2016-03-23 10:18:46 -0700

    remove unused scrolling

--- init.el
+++ init.el
@@ -266,18 +266,6 @@
 (setq split-height-threshold nil)
 (setq split-width-threshold 150)
 
-;; scroll by 1 line at the end of the file
-;;(setq scroll-step 1
-;;      scroll-conservatively 10000)
-;;;; set mouse wheel to scroll one line at a time
-;;(setq mouse-wheel-progressive-speed nil)
-;;;; scroll one line at a time (less "jumpy" than defaults)
-;;(setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
-;;;; don't accelerate scrolling
-;;(setq mouse-wheel-progressive-speed nil)
-;;;; scroll window under mouse
-;;(setq mouse-wheel-follow-mouse 't)
-
 (use-package smooth-scrolling
   :config
   (set-variable 'smooth-scroll-margin 5)
