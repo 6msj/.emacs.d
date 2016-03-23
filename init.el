b6bc6921e45015996dc369d5d773c91af78bcefa
James Nguyen <james@whoshere.net>
2016-03-22 20:24:19 -0700

    magit status opening in same buffer (usually)

--- init.el
+++ init.el
@@ -234,7 +234,29 @@
   :config
   (setq magit-repository-directories '("~/Developer" "~/.emacs.d" "~/.vim"))
   (setq magit-refresh-status-buffer nil)
-  (setq magit-completing-read-function 'ivy-completing-read))
+  (setq magit-completing-read-function 'ivy-completing-read)
+
+  ;; https://github.com/magit/magit/issues/2541 (tweaked)
+  ;; single window or special magit modes -> open in other window
+  (setq magit-display-buffer-function
+        (lambda (buffer)
+          (display-buffer
+           buffer
+           (cond
+            ((eq (count-windows) 1)
+             nil)
+            ((and (derived-mode-p 'magit-mode)
+                  (eq (with-current-buffer buffer major-mode)
+                      'magit-status-mode))
+             nil)
+            ((memq (with-current-buffer buffer major-mode)
+                   '(magit-process-mode
+                     magit-revision-mode
+                     magit-diff-mode
+                     magit-stash-mode))
+             nil)
+            (t
+             '(display-buffer-same-window)))))))
 
 ;; prefer vertical splits
 ;; https://stackoverflow.com/questions/2081577/setting-emacs-split-to-horizontal
