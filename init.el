6dff110e606837570ec4bd8d325d85e7f7732036
James Nguyen <james@whoshere.net>
2016-04-01 17:01:42 -0700

    add .dotfiles to repo list

--- init.el
+++ init.el
@@ -258,7 +258,11 @@
 (use-package magit
   :commands (magit-autoload-commands)
   :config
-  (setq magit-repository-directories '("~/Developer" "~/.emacs.d" "~/.vim" "~/.zsh"))
+  (setq magit-repository-directories '("~/Developer"
+                                       "~/.emacs.d"
+                                       "~/.vim"
+                                       "~/.dotfiles"
+                                       "~/.zsh"))
   (setq magit-refresh-status-buffer nil)
   (setq magit-completing-read-function 'ivy-completing-read)
 
