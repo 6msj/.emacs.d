5368b75f3329931c2c51aa07b1399c57736c7ba8
James Nguyen <james@whoshere.net>
2016-04-12 17:06:44 -0700

    hook on visual line mode

--- init.el
+++ init.el
@@ -377,7 +377,7 @@ Otherwise the symbol 'solarized won't yet be defined."
       scroll-conservatively 10000
       scroll-preserve-screen-position 1)
 
-(visual-line-mode 1) ; wraps line when it reaches end
+(add-hook 'text-mode-hook 'visual-line-mode) ; wraps line when it reaches end
 
 (use-package git-gutter+
   :diminish git-gutter+-mode
